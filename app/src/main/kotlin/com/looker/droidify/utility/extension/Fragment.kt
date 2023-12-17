9efd9725d03549d0f467d59c5a45df7996ab6af5
harvey186 <harvey186@hotmail.com>
2023-12-17 11:41:33 +0100

    x

--- app/src/main/kotlin/com/looker/droidify/utility/extension/Fragment.kt
+++
@@ -1,7 +0,0 @@
-package com.looker.droidify.utility.extension
-
-import androidx.fragment.app.Fragment
-import com.looker.droidify.ScreenActivity
-
-inline val Fragment.screenActivity: ScreenActivity
-    get() = requireActivity() as ScreenActivity
